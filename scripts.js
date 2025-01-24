a078f92a0f72dce5ad390dddb33721bc63bf04e5
Daniel <161630399+dws1114@users.noreply.github.com>
2025-01-24 12:06:15 -0500

    Create reset button

--- scripts.js
+++ scripts.js
@@ -5,11 +5,20 @@ function createDivs(numDivs) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "pixel")
         newDiv.setAttribute("style", "border: 1px solid black;")
-        newDiv.style.height = "50px";
-        newDiv.style.width = "50px";
+        newDiv.style.height = "20px";
+        newDiv.style.width = "20px";
 
         container.appendChild(newDiv);
     }
 }
 
 createDivs(256);
+
+const pixels = document.querySelectorAll(".pixel");
+
+for (let pixel of pixels) {
+    pixel.addEventListener("mouseover", () => {
+        pixel.style.background = "black";
+    })
+}
+
